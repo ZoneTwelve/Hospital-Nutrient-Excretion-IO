54ca4feaf9256ec4d5d28e9384a2be5cbece6a5f
ZoneTwelve <zonetwelve159@gmail.com>
2021-12-18 15:32:05 +0800

    [update] UI design

--- api/models.py
+++ api/models.py
@@ -12,16 +12,20 @@ class Poop_opt( models.Model ):
   name       = models.TextField( blank=False )
 
 class NE_Record( models.Model ): # Nutrient & Excretion Record
+  user_id    = models.IntegerField( default=-1 )
+
   food_name  = models.IntegerField( ) # Ref Foods.id
   food_cap   = models.IntegerField( )
+  food_state = models.TextField( default='' )
   
   water_cap  = models.IntegerField( )
   
-  poop       = models.IntegerField( ) # choose
-  poop_state = models.TextField( )
-  
   pee        = models.IntegerField( ) # choose
-  pee_state  = models.TextField( )
+  pee_cap    = models.IntegerField( default=0 )
+  pee_state  = models.TextField( default='' )
 
+  poop       = models.IntegerField( ) # choose
+  poop_state = models.TextField( default='' )
+  
   edit_date   = models.DateField( auto_now=True )
   update_date = models.DateField( auto_now_add=True )
--- applications/public/src/js/index.js
+++ applications/public/src/js/index.js
@@ -1,9 +1,10 @@
 window.onload = function( ){
   let d = new Date( );
-  document.querySelector("#date-picker").value = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
+  document.querySelector("#date-picker").value = `${d.getFullYear()}-${zeroStuffing(2, d.getMonth())}-${zeroStuffing(2, d.getDate())}`;
   document.querySelector("#add-btn").onclick = addNewColumns;
   document.querySelector("#date-picker").onchange = loadColumns;
   document.querySelector("#close-new-columns").onclick = closeNewColumns;
+  document.querySelector("#new-option").onchange = showActiveOption;
   // document.querySelector("#new-columns").onclick = closeNewColumns;
   loadColumns( );
 }
@@ -11,9 +12,13 @@ window.onload = function( ){
 function loadColumns( ){
 }
 
+function zeroStuffing( len, str ){
+  return (new Array(len).fill(0).join('') + str).substr(-len);
+}
+
 function addNewColumns( ){
   let d = new Date();
-  document.querySelector("#timing").value = `${d.getHours()}:${d.getMinutes()}`;
+  document.querySelector("#timing").value = `${zeroStuffing(2, d.getHours())}:${zeroStuffing(2, d.getMinutes())}`;
   document.querySelector("#new-columns").classList.remove("new-columns-hide");
   document.querySelector("#new-columns").classList.add("new-columns-show");
 }
@@ -22,4 +27,16 @@ function closeNewColumns( ){
   document.querySelector("#new-columns").classList.add("new-columns-hide");
   document.querySelector("#new-columns").classList.remove("new-columns-show");
 
+}
+
+function showActiveOption( ){
+  let opts = document.querySelectorAll(".ctrl-opts");
+
+  for(let opt of opts){
+    opt.classList.add("ctrl-opts-hide");
+  }
+  let el = document.querySelector(`#fill-${this.value}`);
+  if( el ){
+    document.querySelector(`#fill-${this.value}`).classList.remove('ctrl-opts-hide');
+  }
 }
